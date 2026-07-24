9cb5d4c95091a6401542bd11c81af7c12f5ee517
marfusha1986 <afag@imanov.me>
2026-07-24 08:10:16 +0300

    Butonlar ve kolonlar düzenlendi

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ class DataAnalyseLogic:
         self.df = self.df.convert_dtypes()
         return list(self.df.columns)
 
+
     def train(self, target, features):
         X = self.df[features]
         y = self.df[target]
@@ -59,6 +60,56 @@ class DataAnalyseLogic:
         plt.title('Korelasyon Matrisi Heatmap')
         plt.show()
 
+    def plot_scatter(self,x_col,y_col,color_col=None):
+        plt.figure(figsize=(6,4))
+        #Eğer renk kolonunu seçmişse ve kolon kategorikse
+        if color_col and color_col in self.df.columns:
+            unique_vals = self.df[color_col].unique()
+
+            for val in unique_vals:
+                subset = self.df[self.df[color_col] == val]
+                plt.scatter(subset[x_col],subset[y_col],label=str(val))
+            plt.legend()
+        else:
+            plt.scatter(self.df[x_col],self.df[y_col])
+
+        plt.xlabel(x_col)
+        plt.ylabel(y_col)
+        plt.title(f"{x_col} vs {y_col}")
+        plt.show()
+
+    def plot_line(self,x_col,y_col):
+        plt.figure(figsize=(6,4))
+        plt.plot(self.df[x_col],self.df[y_col])
+        plt.xlabel(x_col)
+        plt.ylabel(y_col)
+        plt.title(f"{x_col} vs {y_col} (Line Plot)")
+        plt.show()
+
+    def plot_box(self,col):
+        plt.figure(figsize=(6,4))
+        plt.boxplot(self.df[col])
+        plt.title(f"{col} Boxplot")
+        plt.show()
+
+    def plot_hist(self,x_col,bins=30):
+        if not pd.api.types.is_numeric_dtype(self.df[x_col]):
+            print(f"{x_col} kolonunun tipi sayısal değil,histogram çizilemez!")
+            return
+
+        plt.figure(figsize=(6,4))
+        plt.hist(self.df[x_col],bins=bins,color='skyblue',edgecolor='black')
+        plt.xlabel(x_col)
+        plt.ylabel("Frekans")
+        plt.title(f"{x_col} Histogram")
+        plt.show()
+
+    def get_numeric_columns(self):
+        return list(self.df.select_dtypes(include=['float64','int64']).columns)
+
+    def get_categorical_columns(self):
+        return list(self.df.select_dtypes(include=["string","object"]).columns)
+
 
 class DataAnalyseGUI:
     def __init__(self, root):
@@ -68,60 +119,97 @@ class DataAnalyseGUI:
 
         self.logic = DataAnalyseLogic()
 
-        # VERİ YÜKLEME
-        self.button_upload = tk.Button(self.window, text='Veri Seti Yükle', command=self.file_upload)
-        self.button_upload.pack(pady=10)
+        #---- Sol taraf----
+        self.frame_left = tk.Frame(self.window)
+        self.frame_left.pack(side="left",fill="y",padx=10,pady=10)
 
-        # SÜTUN TİPLERİ
-        self.lbl_types = tk.Label(self.window, text='Sütun Türleri:')
-        self.lbl_types.pack()
-        self.list_types = tk.Listbox(self.window, height=6)
-        self.list_types.pack(pady=5)
+        # ----VERİ YÜKLEME---
+        self.button_upload = tk.Button(self.frame_left, text='Veri Seti Yükle', command=self.file_upload)
+        self.button_upload.grid(row=0, column=0, pady=10)
 
-        # EKSİK DEĞER ANALİZİ
-        self.button_null = tk.Button(self.window, text='Eksik Değer Analizi', command=self.null_analysis_gui)
-        self.button_null.pack(pady=10)
+        # ---SÜTUN TİPLERİ----
+        self.lbl_types = tk.Label(self.frame_left, text='Sütun Türleri:')
+        self.lbl_types.grid(row=1,column=0,sticky="w")
 
-        # EKSİK DEĞER DOLDURMA
-        self.lbl_fill = tk.Label(self.window, text='Eksik Değer Doldurma:')
-        self.lbl_fill.pack()
+        self.list_types = tk.Listbox(self.frame_left, height=6)
+        self.list_types.grid(row=2,column=0,pady=5)
 
-        self.combo_fill_col = ttk.Combobox(self.window)
-        self.combo_fill_col.pack(pady=3)
+        self.button_null = tk.Button(self.frame_left, text='Eksik Değer Analizi', command=self.null_analysis_gui)
+        self.button_null.grid(row=3, column=0, pady=10)
 
-        self.combo_fill_type = ttk.Combobox(self.window, values=['mean', 'median', 'mode'])
+
+        # ---EKSİK DEĞER DOLDURMA----
+        self.lbl_fill = tk.Label(self.frame_left, text='Eksik Değer Doldurma:')
+        self.lbl_fill.grid(row=4,column=0,pady=5)
+
+        self.combo_fill_col = ttk.Combobox(self.frame_left,state="readonly")
+        self.combo_fill_col.grid(row=5,column=0,pady=5)
+
+        self.combo_fill_type = ttk.Combobox(self.frame_left, values=['mean', 'median', 'mode'])
         self.combo_fill_type.set('mean')
-        self.combo_fill_type.pack()
+        self.combo_fill_type.grid(row=6,column=0,pady=5)
 
-        self.button_fill = tk.Button(self.window, text='Eksik Değer Doldur', command=self.fill_nulls_gui)
-        self.button_fill.pack(pady=10)
+        self.button_fill = tk.Button(self.frame_left, text='Eksik Değer Doldur', command=self.fill_nulls_gui)
+        self.button_fill.grid(row=7, column=0, pady=10)
 
-        # KORELASYON MATRİSİ
-        self.button_corr = tk.Button(self.window, text='Korelasyon Matrisi (Heatmap)', command=self.show_corr_gui)
-        self.button_corr.pack(pady=10)
+        # ---KORELASYON MATRİSİ---
+        self.button_corr = tk.Button(self.frame_left, text='Korelasyon Matrisi (Heatmap)', command=self.show_corr_gui)
+        self.button_corr.grid(row=8, column=0, pady=10)
+
+
+        #---Sağ taraf----
+        self.frame_right = tk.Frame(self.window)
+        self.frame_right.pack(side="right",fill="y",padx=10,pady=10)
+
+        self.combo_x = ttk.Combobox(self.frame_right, state="readonly")
+        self.combo_x.grid(row=0, column=0, pady=5)
+
+        self.combo_y = ttk.Combobox(self.frame_right, state="readonly")
+        self.combo_y.grid(row=1, column=0, pady=5)
+
+        #--- Renk Kolon Seçimi ---
+        self.lbl_color = tk.Label(self.frame_right,text="Renk Kolonu:")
+        self.lbl_color.grid(row=2,column=0,pady=5)
+
+        self.combo_color=ttk.Combobox(self.frame_right,state='readonly')
+        self.combo_color.grid(row=3,column=0,pady=5)
+
+        #----GRAFİK ÇİZİMİ----
+        self.lbl_plot_type = tk.Label(self.frame_right,text="Grafik Türü: ")
+        self.lbl_plot_type.grid(row=4,column=0,pady=5)
+
+        self.combo_plot_type = ttk.Combobox(self.frame_right,state="readonly",
+                                            values=['scatter','line','boxplot','histogram'])
+        self.combo_plot_type.grid(row=5,column=0,pady=5)
+        self.combo_plot_type.set('scatter') #varsayılan
+
+        self.btn_scatter = tk.Button(self.frame_right,text="Grafik Çiz", command=self.draw_plot)
+        self.btn_scatter.grid(row=6,column=0,pady=10)
 
         # HEDEF KOLON
-        self.lbl_target = tk.Label(self.window, text='Hedef Kolon (Label):')
-        self.lbl_target.pack()
-        self.combo_target = ttk.Combobox(self.window)
-        self.combo_target.pack(pady=3)
+        self.lbl_target = tk.Label(self.frame_right, text='Hedef Kolon (Label):')
+        self.lbl_target.grid(row=7, column=0, pady=5)
+
+        self.combo_target = ttk.Combobox(self.frame_right)
+        self.combo_target.grid(row=8, column=0, pady=5)
 
         # ÖZELLİK KOLONLARI
-        self.lbl_features = tk.Label(self.window, text='Özellik Kolonları (Features):')
-        self.lbl_features.pack()
-        self.list_features = tk.Listbox(self.window, selectmode='multiple', height=10)
-        self.list_features.pack(pady=3)
+        self.lbl_features = tk.Label(self.frame_right, text='Özellik Kolonları (Features):')
+        self.lbl_features.grid(row=9, column=0, pady=5)
 
-        # MODEL EĞİTME
-        self.button_train = tk.Button(self.window, text='Modeli Eğit', command=self.train_model)
-        self.button_train.pack(pady=10)
+        self.list_features = tk.Listbox(self.frame_right, selectmode='multiple', height=10)
+        self.list_features.grid(row=10, column=0, pady=5)
 
-        # TAHMİN
-        self.button_predict = tk.Button(self.window, text='Tahmin Yap', command=self.predict_row)
-        self.button_predict.pack(pady=10)
+        self.lbl_score = tk.Label(self.frame_right, text="Henüz model eğitilmedi!")
+        self.lbl_score.grid(row=11, column=0, pady=5)
 
-        self.lbl_score = tk.Label(self.window,text="Henüz model eğitilmedi!")
-        self.lbl_score.pack()
+        # ---MODEL EĞİTME---
+        self.button_train = tk.Button(self.frame_right, text='Modeli Eğit', command=self.train_model)
+        self.button_train.grid(row=12, column=0, pady=10)
+
+        # ---TAHMİN---
+        self.button_predict = tk.Button(self.frame_right, text='Tahmin Yap', command=self.predict_row)
+        self.button_predict.grid(row=13,column=0,pady=10)
 
     def file_upload(self):
         file_path = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
@@ -129,25 +217,45 @@ class DataAnalyseGUI:
             return
 
         columns = self.logic.load_data(file_path)
-        # Target sadece kategorik kolonlar
+        # ---Target sadece kategorik kolonlar---
         categorical_cols = self.logic.df.select_dtypes(include=['object','string']).columns
         self.combo_target['values'] = list(categorical_cols)
 
-        # Feature listesine sadece sayısal kolonlar
+        # ---Feature listesine sadece sayısal kolonlar---
         numeric_cols = self.logic.df.select_dtypes(include=['float64', 'int64']).columns
         self.list_features.delete(0, tk.END)
         for col in numeric_cols:
             self.list_features.insert(tk.END, col)
 
-        # Eksik değer doldurma kolonları
+        # ---Eksik değer doldurma kolonları----
         self.combo_fill_col['values'] = columns
 
-        # Sütun tipleri
+        # ---Sütun tipleri---
         self.list_types.delete(0, tk.END)
         for col, dtype in self.logic.df.dtypes.items():
             self.list_types.insert(tk.END, f"{col} ---> {dtype}")
 
         print("Veri başarıyla yüklendi, sütunlar kutulara doldu!")
+        cols = list(self.logic.df.columns)
+        self.combo_x['values'] = cols
+        self.combo_y['values'] = cols
+
+        #---RENK SEÇİMİ---
+        cat_cols = self.logic.get_categorical_columns()
+        self.combo_color['values']=cat_cols
+        self.combo_color.set("")
+
+        if len(cat_cols) == 0:
+            self.combo_color.config(state="disabled")
+        else:
+            self.combo_color.config(state="readonly")
+
+        #Sayısal Kolon Kontrolu
+        numeric_cols = self.logic.get_numeric_columns()
+        if len(numeric_cols) < 2:
+            self.btn_scatter.config(state="disabled")
+        else:
+            self.btn_scatter.config(state="normal")
 
     def train_model(self):
         target = self.combo_target.get()
@@ -163,6 +271,7 @@ class DataAnalyseGUI:
         accuracy = self.logic.train(target, features)
         self.lbl_score.config(text=f"Model Başarı Oranı: {accuracy:.2f}")
         print(f"Model Başarı oranı: {accuracy:.2f}")
+
     def predict_row(self):
         if self.logic.model is None:
             print("Lütfen önce modeli eğitin!")
@@ -205,6 +314,12 @@ class DataAnalyseGUI:
             print("Lütfen bir kolon seçin!")
             return
 
+        #Sayısal Kolon Kontrolü(Eğer sayısal değise mean kullanılamaz
+        if method in ["mean","median"]:
+            if not pd.api.types.is_numeric_dtype(self.logic.df[col]):
+                print(f"{col} kolonunun tipi sayısal değil,'{method}' uygulanamaz!")
+                return
+
         if method == "mean":
             value = self.logic.df[col].mean()
         elif method == "median":
@@ -215,6 +330,27 @@ class DataAnalyseGUI:
         self.logic.df[col] = self.logic.df[col].fillna(value)
         print(f"{col} kolonundaki eksik değerler '{method}' ile dolduruldu.")
 
+    def scatter_plot(self):
+        x = self.combo_x.get()
+        y = self.combo_y.get()
+        color=self.combo_color.get()
+
+        self.logic.plot_scatter(x,y,color)
+
+    def draw_plot(self):
+        plot_type = self.combo_plot_type.get()
+        x = self.combo_x.get()
+        y = self.combo_y.get()
+        color = self.combo_color.get()
+
+        if plot_type == "scatter":
+            self.logic.plot_scatter(x,y,color)
+        elif plot_type == "line":
+            self.logic.plot_line(x,y)
+        elif plot_type == "boxplot":
+            self.logic.plot_box(x)
+        elif plot_type == "histogram":
+            self.logic.plot_hist(x)
 
 if __name__ == "__main__":
     root = tk.Tk()
